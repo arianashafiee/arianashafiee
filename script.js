e9065bc719084be6fff0a5a6d43d5d4768f5eb5e
arianashafiee <ashafie1@jhu.edu>
2025-01-17 01:10:24 -0500

    .

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
     $(window).scroll(function () {
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass('sticky');
             $('.dropdown').addClass('sticky');
-
-        }
-        else{
+        } else {
             $('.navbar').removeClass('sticky');
             $('.dropdown').removeClass('sticky');
-
-
         }
     });
 
-    //Script to toggle menu and navbar
-    $('.navbar .menu li a').click(function(){
+    // Script to toggle menu and navbar
+    $('.navbar .menu li a').click(function () {
         // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
+        $('html').css('scrollBehavior', 'smooth');
     });
-    
-    $('.menu-btn').click(function() {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
 
-    })
-    $('.navbar .menu li a').click(function() {
-        $('.navbar .menu').removeClass("active");
-        $('.menu-btn i').removeClass("active");
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass('active');
+        $('.menu-btn i').toggleClass('active');
+    });
+    $('.navbar .menu li a').click(function () {
+        $('.navbar .menu').removeClass('active');
+        $('.menu-btn i').removeClass('active');
+    });
 
-    })
-        //Typing Script
-
-        var typed = new Typed(".typing", {
-            strings: [" SWE intern", "Research assitant @ Corrie",  "Former product @ ServiceNow", "Founding UX @ Otto", "Former UX @ Infernoguard", "Former UX, SWE @ Inbound"],
-            typeSpeed: 140,
-            backSpeed: 100,
-            loop: true
-        });
+    // Typing Script
+    var typed = new Typed('.typing', {
+        strings: [
+            ' SWE intern',
+            'Research assistant @ Corrie',
+            'Former product @ ServiceNow',
+            'Founding UX @ Otto',
+            'Former UX @ Infernoguard',
+            'Former UX, SWE @ Inbound',
+        ],
+        typeSpeed: 140,
+        backSpeed: 100,
+        loop: true,
+    });
 });
